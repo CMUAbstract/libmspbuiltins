2ea0d5bd63061889e1a1a0f781f6668c9de38371
Alexei Colin <ac@alexeicolin.com>
2015-10-23 21:43:29 -0400

    fake math operation intrinsics not impl in Clang

--- src/include/msp-builtins.h
+++ src/include/msp-builtins.h
@@ -1,6 +1,8 @@
 #ifndef _MSP_BUILTINS_H
 #define _MSP_BUILTINS_H
 
+#include <stdint.h>
+
 #ifdef __clang__
 
 #define __delay_cycles(n) \
@@ -21,5 +23,11 @@
     )
 
 #endif // __clang__
+
+// These are fake "built-ins" for Clang: implemented in C in a trivial way, and
+// compiled with gcc. We do this because it is easier than implementing actual
+// builtins Clang calls that come from compiler-rt (few are implemented there).
+uint32_t mspbuiltins_mult32(uint32_t a, uint32_t b);
+uint16_t mspbuiltins_div16(uint16_t a, uint16_t b);
 
 #endif // _MSP_BUILTINS_H
